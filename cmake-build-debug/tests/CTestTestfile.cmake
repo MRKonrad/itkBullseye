8a855829c9fdb22d238a3e82258bf4e16ffd31dd
Konrad Werys <konradwerys2@gmail.com>
2019-04-29 13:54:35 +0100

    fix: trying to fix the notifications v3

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kwerys/Code/itkBullseye/tests
-# Build directory: /Users/kwerys/Code/itkBullseye/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ctest_tests "/Users/kwerys/Code/itkBullseye/cmake-build-debug/tests/itkBullseyeTests")
